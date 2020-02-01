ad2314ba335fa55acb0e029f79a9d045bf5e1555
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-01 12:49:39 -0800

    Cleaned up white space

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MainContainer = ({ children }) => {
-  // console.log(children);
-
-  return (
-    <main>
-      <div className="inner-container">
-        {children}
-      </div>
-    </main>
-  );
-}
+const MainContainer = ({ children }) => (
+  <main>
+    <div className="inner-container">
+      {children}
+    </div>
+  </main>
+);
 
 MainContainer.propTypes = {
-  children: PropTypes.element,
+  children: PropTypes.arrayOf(PropTypes.element),
 };
 
 export default MainContainer;
