6472e35526b3b033d6bd18c8b56adb97b57e120a
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-01 13:01:01 -0800

    Refactored code and added new props

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,55 +1,179 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import MainContainer from './MainContainer';
 import TaskHeading from './TaskHeading';
+import WordBox from './WordBox';
 import ArticleButtons from './ArticleButtons';
 import CheckAnswers from './CheckAnswers';
+import AnswerBox from './AnswerBox';
 
-const Task = ({ taskObj }) => {
-  const {
-    lang, articleType, articles, selectedArticle, currentWord, currentEnglish,
-    actions, answer, answerClass, checkAnswerLabel, answerText,
-  } = taskObj;
-  const { handleArticleClick, handleCheckAnswer } = actions;
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    const { wordList } = this.props;
 
-  return (
-    <MainContainer>
-      <TaskHeading
-        lang={lang}
-        articleType={articleType}
-      />
+    this.state = {
+      currentIndex: 0,
+      wordList,
+      selectedArticle: '',
+      buttonLabel: 'check',
+      answer: '',
+      answerClass: '',
+      answerText: '',
+    };
+  }
 
-      <div className="word-box">
-        <div>
-          <p>{currentEnglish}</p>
-          <span className="word-box__article">{selectedArticle}</span>
-          <span className="word-box__word">{currentWord}</span>
-        </div>
-      </div>
+  componentDidUpdate(prevProps) {
+    const { articleType, wordList } = this.props;
 
-      <ArticleButtons
-        articles={articles}
-        handleArticleClick={handleArticleClick}
-      />
+    if (wordList !== prevProps.wordList || articleType !== prevProps.articleType) {
+      this.resetComponent();
+    }
+  }
 
-      <CheckAnswers
-        label={checkAnswerLabel}
-        handleCheckAnswer={handleCheckAnswer}
-      />
+  resetComponent = () => {
+    const { wordList } = this.props;
+    this.setState({
+      currentIndex: 0,
+      wordList,
+      selectedArticle: '',
+      answer: '',
+      answerClass: '',
+      answerText: '',
+    });
+  }
 
-      <div className={`answer-box ${answerClass}`}>
-        <div>
-          <p className="answer-box__text">{answerText}</p>
-          <p className="answer-box__answer">{answer}</p>
-        </div>
-      </div>
-    </MainContainer>
-  );
-};
+  clearLastAnswer = () => {
+    this.setState({
+      selectedArticle: '',
+      answer: '',
+      answerClass: '',
+      answerText: '',
+    });
+  }
+
+  handleArticleClick = (selectedArticle) => {
+    this.setState({ selectedArticle });
+  }
+
+  handleNextClick = () => {
+    this.clearLastAnswer();
+    this.changeButtonLabel('check');
+
+    this.setState((prevState) => {
+      if (prevState.currentIndex < prevState.wordList.length - 1) {
+        return { currentIndex: prevState.currentIndex + 1 };
+      }
+      return { currentIndex: prevState.currentIndex };
+    });
+  }
+
+  handleCheckClick = () => {
+    const { currentIndex, wordList, selectedArticle } = this.state;
+
+    if (selectedArticle === '') {
+      return;
+    }
+
+    this.checkAnswer();
+
+    if (currentIndex < wordList.length - 1) {
+      this.changeButtonLabel('next');
+    } else {
+      this.changeButtonLabel('finish');
+    }
+  }
+
+  checkAnswer = () => {
+    const { currentIndex, selectedArticle, wordList } = this.state;
+    const { articleType } = this.props;
+
+    const answerName = `${articleType}Answer`;
+    const tempWordList = wordList;
+    let answerClass;
+    const answer = `${wordList[currentIndex][answerName]} ${wordList[currentIndex].word}`;
+    let answerText;
+    let isAnswerCorrect;
+
+    if (selectedArticle === wordList[currentIndex][answerName]) {
+      isAnswerCorrect = true;
+      answerClass = 'answer-correct';
+      answerText = 'Your answer is correct';
+    } else {
+      isAnswerCorrect = false;
+      answerClass = 'answer-incorrect';
+      answerText = 'The correct answer is';
+      tempWordList.push(wordList[currentIndex]);
+    }
+
+    this.setState({
+      wordList: tempWordList,
+      answerClass,
+      answer,
+      answerText,
+    });
+
+    return isAnswerCorrect;
+  }
+
+  handleFinishClick = () => {
+    const { changePage } = this.props;
+    changePage('final');
+  }
+
+  changeButtonLabel = (buttonLabel) => {
+    // const buttonLabelNames = ['check', 'next', 'continue'];
+    this.setState({ buttonLabel });
+  }
+
+  render() {
+    const {
+      buttonLabel, selectedArticle, currentIndex,
+      answer, answerClass, answerText,
+    } = this.state;
+    const { articleType, wordList, lang } = this.props;
+
+    return (
+      <MainContainer>
+        <TaskHeading
+          lang={lang}
+          articleType={articleType}
+        />
+
+        <WordBox
+          currentEnglish={wordList[currentIndex].english}
+          selectedArticle={selectedArticle}
+          currentWord={wordList[currentIndex].word}
+        />
+
+        <ArticleButtons
+          articles={wordList[currentIndex][articleType]}
+          handleArticleClick={this.handleArticleClick}
+        />
+
+        <CheckAnswers
+          buttonLabel={buttonLabel}
+          handleCheckClick={this.handleCheckClick}
+          handleNextClick={this.handleNextClick}
+          handleFinishClick={this.handleFinishClick}
+        />
+
+        <AnswerBox
+          answerClass={answerClass}
+          answerText={answerText}
+          answer={answer}
+        />
+      </MainContainer>
+    );
+  }
+}
 
 Task.propTypes = {
-  taskObj: PropTypes.shape(),
+  lang: PropTypes.string,
+  articleType: PropTypes.string,
+  wordList: PropTypes.arrayOf(PropTypes.shape()),
+  changePage: PropTypes.func,
 };
 
 export default Task;
